a65680362dd80f60b72717a405049a066af9ebed
Shirshendu-Pal <shirshendupal9@gmail.com>
2022-07-08 19:13:25 +0530

    checking in server

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 module.exports = {
     isProduction: false || process.env.isProduction,
-    mongoDbUrl: 'mongodb://localhost/oauth_server' || process.env.mongoDbUrl,
+    mongoDbUrl: 'mongodb+srv://shaon:shaon123@cluster0.zhl1ddx.mongodb.net/oauth-testi?retryWrites=true&w=majority' || process.env.mongoDbUrl,
     salt: 'a5828e9d6052dc3b14a93e07a5932dd9' || process.env.salt
 };
